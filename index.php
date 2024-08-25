99442bf8dd9a12fc64a458a9bb9f54e60e5f8771
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-08-25 14:49:42 +0200

    feat: page structure completed and stylized

--- index.php
+++ index.php
@@ -8,9 +8,53 @@
     <title>TOBOlist</title>
 
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
 </head>
 
 <body>
+    <div class="page-wrapper">
+        <div>
+            <div class="content">
+                <header>
+                    <h1>
+                        TOBOlist
+                    </h1>
+                </header>
+
+                <main>
+                    <ul class="todo-list">
+                        <li class="event">
+                            <h2 class="event-name">
+                                testtesttesttest
+                            </h2>
+
+                            <menu class="event-options">
+                                <li>
+                                    <button>
+                                        <i class="fa-solid fa-eye"></i>
+                                    </button>
+                                </li>
+
+                                <li>
+                                    <button>
+                                        <i class="fa-solid fa-check"></i>
+
+                                        <!-- condizione per mettere altro testo -->
+                                    </button>
+                                </li>
+
+                                <li>
+                                    <button>
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                </li>
+                            </menu>
+                        </li>
+                    </ul>
+                </main>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
